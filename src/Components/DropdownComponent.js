a696f84833e952a6e69c446969d13fa9a4bd3247
unknown <45riteshyadav@gmail.com>
2021-11-18 10:34:25 +0530

    changes

--- src/Components/DropdownComponent.js
+++ src/Components/DropdownComponent.js
@@ -4,118 +4,196 @@ import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
 // import '../../index.css';
 import { InputText } from "primereact/inputtext";
+import { Card } from "primereact/card";
 
 import React, { useState, useEffect, useRef } from "react";
 import { Dropdown } from "primereact/dropdown";
 // import './DropdownDemo.css';
 
-import data from "./data.json";
-
 function DropdownComponent(props) {
-  const [inputItems, setiInputItems] = useState([
-    { title1: "Select Country", title2: "Select Language" },
+  const [inputList, setInputList] = useState([
+    { firstName: "", lastName: "", designation: "" },
   ]);
-  const [lazyLoading, setLazyLoading] = useState(false);
-  const [city, setCity] = useState(null);
-  const [cityList, setCityList] = useState(null);
+  const [leaveInputs, setLeaveInputs] = useState([
+    {
+      NoOfDays: "",
+      LeaveLeft: "",
+    },
+  ]);
+  const [formType, setFormType] = useState([{ name: "Employee", code: "EM" }]);
 
-  const [selectedCity2, setSelectedCity2] = useState(null);
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const [selectedGroupedCity, setSelectedGroupedCity] = useState(null);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedItem2, setSelectedItem2] = useState(null);
+  const [city, setCity] = useState("");
 
-  let loadLazyTimeout = useRef(null);
+  const [menu, setMenu] = useState([
+    { label: "Employee", value: "Employee" },
+    { label: "Leave", value: "Leave" },
+  ]);
+  const cities = [
+    { name: "Employee", code: "EM" },
+    { name: "Leave", code: "LV" },
+  ];
 
-  const [country, setCountry] = useState("");
-  const [lang, setLang] = useState(null);
-  const [langList, setLangList] = useState([]);
-  const [population, setPopulation] = useState("");
-
-  // handle change event of the country dropdown
-  const handleCountryChange = (obj) => {
-    setCountry(obj.value.region);
-    setCityList(obj.value.cities);
-    setLangList(obj.value.languages);
-    setPopulation(obj.value.population);
-    console.log(langList);
-    setLang(null);
-  };
-
-  // handle change event of the language dropdown
-  const handleLanguageChange = (obj) => {
-    console.log(obj);
-    setLang(obj.value.name);
-  };
-
-  const handleCityChange = (obj) => {
-    console.log(obj);
-    setCity(obj.value.label);
-  };
-
-  // generate the link when both dropdowns are selected
-
-  // const handleAddClick = () => {
-  //   setiInputItems([
-  //     ...inputItems,
-  //     {
-  //       title1: "Select Country",
-  //       title2: "Select Language",
-  //       title3: "Population",
-  //     },
-  //   ]);
+  // const handleCityChange = (obj) => {
+  //   console.log(obj);
+  //   setCity(obj.value.label);
   // };
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+  const handleLeaveInput = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...leaveInputs];
+    list[index][name] = value;
+    setLeaveInputs(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([
+      ...inputList,
+      { firstName: "", lastName: "", designation: "" },
+    ]);
+  };
+  const onFormChange = (e) => {
+    setFormType(e.value);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>
-        <h5>{props.data.title1}</h5>
+    <Card style={{ width: "800px", margin: "20px 0" }}>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="dropdown">
+          <h3>
+            <a href="https://cluemediator.com">Form Details</a>
+          </h3>
+          <Dropdown
+            value={formType}
+            options={cities}
+            onChange={onFormChange}
+            optionLabel="name"
+            placeholder="Select Form"
+          />
 
-        <Dropdown
-          options={data}
-          optionLabel={(x) => x.region}
-          optionValue={(x) => x}
-          placeholder={country}
-          value={country}
-          // disabled={item.parentSelected=='Value'}
-          onChange={(e) => handleCountryChange(e)}
-        />
-      </div>
-      <div>
-        <h5>{props.data.title2}</h5>
+          {formType.name === "Employee"
+            ? inputList.map((x, i) => {
+                return (
+                  <div className="box">
+                    <div className="inputBox">
+                      <h5>First Name </h5>
+                      <InputText
+                        name="firstName"
+                        placeholder="First Name"
+                        value={x.firstName}
+                        onChange={(e) => handleInputChange(e, i)}
+                      />
+                    </div>
 
-        <Dropdown
-          options={cityList}
-          optionLabel={(x) => x.label}
-          optionValue={(x) => x}
-          placeholder={city}
-          value={city}
-          // disabled={item.parentSelected=='Value'}
-          onChange={(e) => handleCityChange(e)}
-        />
+                    <div className="inputBox">
+                      <h5>Last Name </h5>
+
+                      <InputText
+                        className="ml10"
+                        name="lastName"
+                        placeholder="Last Name"
+                        value={x.lastName}
+                        onChange={(e) => handleInputChange(e, i)}
+                      />
+                    </div>
+
+                    <div className="inputBox">
+                      {" "}
+                      <h5>Designation</h5>
+                      <InputText
+                        className="ml10"
+                        name="designation"
+                        placeholder="Designation"
+                        value={x.designation}
+                        onChange={(e) => handleInputChange(e, i)}
+                      />
+                    </div>
+                    {/* 
+                  <div className="btn-box">
+                    {inputList.length !== 1 && (
+                      <button
+                        className="mr10"
+                        onClick={() => handleRemoveClick(i)}
+                      >
+                        Remove
+                      </button>
+                    )}
+                    {inputList.length - 1 === i && (
+                      <button onClick={handleAddClick}>Add</button>
+                    )}
+                  </div> */}
+                    {/* {city} */}
+                  </div>
+                );
+              })
+            : leaveInputs.map((x, i) => {
+                return (
+                  <div className="box">
+                    <div className="inputBox">
+                      {" "}
+                      <h5>Number of Days </h5>
+                      <InputText
+                        name="NoOfDays"
+                        placeholder="Number Of Days"
+                        value={x.NoOfDays}
+                        onChange={(e) => handleLeaveInput(e, i)}
+                      />
+                    </div>
+
+                    <div className="inputBox">
+                      {" "}
+                      <h5>Leave Left</h5>
+                      <InputText
+                        className="ml10"
+                        name="LeaveLeft"
+                        placeholder="Leave Left"
+                        value={x.LeaveLeft}
+                        onChange={(e) => handleLeaveInput(e, i)}
+                      />
+                    </div>
+                    {/* 
+                  <div className="btn-box">
+                    {inputList.length !== 1 && (
+                      <button
+                        className="mr10"
+                        onClick={() => handleRemoveClick(i)}
+                      >
+                        Remove
+                      </button>
+                    )}
+                    {inputList.length - 1 === i && (
+                      <button onClick={handleAddClick}>Add</button>
+                    )}
+                  </div>
+                  {city} */}
+                  </div>
+                );
+              })}
+          <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+          <div style={{ marginTop: 20 }}>{JSON.stringify(leaveInputs)}</div>
+        </div>
       </div>
-      <div>
-        <h5>{props.data.title3}</h5>
-        <Dropdown
-          options={langList}
-          optionLabel={(x) => x.name}
-          optionValue={(x) => x}
-          placeholder={lang}
-          value={lang}
-          // disabled={item.parentSelected=='Value'}
-          onChange={(e) => handleLanguageChange(e)}
-        />
-      </div>
-      <div>
-        <h5>{props.data.title4}</h5>
-        <InputText value={population} />
-      </div>
-    </div>
+    </Card>
   );
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,58 +1,70 @@
+import { InputText } from "primereact/inputtext";
+import React, { useState } from "react";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
-// import '../../index.css';
 import { Button } from "primereact/button";
-
-import React, { useState, useEffect, useRef } from "react";
-import { Dropdown } from "primereact/dropdown";
-import { Skeleton } from "primereact/skeleton";
-// import './DropdownDemo.css';
-
 import DropdownComponent from "./DropdownComponent";
+// import '../../index.css';
 
 function Main() {
-  const [inputItems, setiInputItems] = useState([
-    {
-      title1: "Select Country",
-      title2: "Select City",
-      title3: "Select Language",
-      title4: "Population",
-    },
+  const [inputList, setInputList] = useState([
+    { firstName: "", lastName: "", designation: "" },
+  ]);
+  const [leaveInputs, setLeaveInputs] = useState([
+      {
+        NoOfDays: "",
+        LeaveLeft: "",
+      },
+    ]),
+    [option, setOption] = useState([""]);
+
+  const [menu, setMenu] = useState([
+    { label: "Employee", value: "Employee" },
+    { label: "Leave", value: "Leave" },
   ]);
 
+  const handleAdd = () => {
+    setOption([...option, ""]);
+  };
+
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+
+  // handle click event of the Add button
   const handleAddClick = () => {
-    setiInputItems([
-      ...inputItems,
-      {
-        title1: "Select Country",
-        title2: "Select City",
-        title3: "Select Language",
-        title4: "Population",
-      },
+    setInputList([
+      ...inputList,
+      { firstName: "", lastName: "", designation: "" },
     ]);
   };
+
   return (
-    <div className="dropdown-demo">
-      <div>
-        {inputItems.map((item, i) => {
-          return (
-            <div>
-              <div className="card">
-                <DropdownComponent data={item} />
-              </div>
-            </div>
-          );
-        })}
-      </div>
+    <div className="main">
       <div className="addButton">
         <Button
-          onClick={handleAddClick}
           icon="pi pi-plus"
           className="p-button-rounded "
+          onClick={handleAdd}
         />
       </div>
+
+      {option.map((a) => (
+        <DropdownComponent />
+      ))}
     </div>
   );
 }
